bd61d1bc7f7a41bce06acd7738235c237af3243c
Leozera2k <leofsousa98@gmail.com>
2024-09-04 10:02:02 -0300

    Update aula02.py

--- linguagem-programacao/aula01/aula02.py
+++ linguagem-programacao/aula01/aula02.py
@@ -1,1 +1,10 @@
-#Estruturas condicionas
+#Estruturas condicionas:
+# <  : Menor que 
+# <= : Menor ou igual que
+# >  : Maior que
+# >= : Maior ou igual que
+# == : Igual
+# != : Diferente
+# is : Identidade do objeto
+# is not : Negação da identidade do objeto
+
