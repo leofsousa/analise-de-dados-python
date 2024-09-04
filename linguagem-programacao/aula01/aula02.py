0361a44cacc7ca6158d6d5b9d2212e47ef39726c
Leozera2k <leofsousa98@gmail.com>
2024-09-04 10:31:30 -0300

    Update aula02.py

--- linguagem-programacao/aula01/aula02.py
+++ linguagem-programacao/aula01/aula02.py
@@ -1,3 +1,4 @@
+import time
 #Estruturas condicionas:
 # <  : Menor que 
 # <= : Menor ou igual que
@@ -12,3 +13,33 @@
 # and = E
 # or = ou
 # not = não
+
+# Vendedor Automático de ingressos de cinema com verificador de faixa etária!
+
+#Verificador de idade:
+idade = int(input('Digite sua idade: '))
+if idade < 12:
+    print(f'Sua idade é {idade}, o filme permitido será o filme 1!')
+elif idade >= 12 and idade < 18:
+    print(f'Sua idade é {idade}, os filmes permitidos serão os filmes 1 e 2!')
+else:
+    print(f'Sua idade é {idade}, você poderá assistir qualquer um dos filmes. 1, 2 ou 3!')
+
+#decisão do filme escolhido
+
+decisao = str(input('Digite o número do filme escolhido:'))
+print(f' O filme escolhido foi o filme {decisao}, vamos verificar se há ingressos disponíveis, um momento...')
+print('Verificando, só mais um momento...')
+time.sleep(10)
+
+
+#Verifica disponibilidade de ingressos:
+
+quantidade_ingressosf1 = 10
+quantidade_ingressosf2 = 6
+quantidade_ingressosf3 = 5
+if decisao == 1:
+    if quantidade_ingressosf1 > 0:
+        print(f'Essa sessão tem {quantidade_ingressosf1} ingressos disponíveis, divirta-se!)')
+    else:
+        print(f'Sentimos muito, os ingressos para essa sessão estão esgotados')
