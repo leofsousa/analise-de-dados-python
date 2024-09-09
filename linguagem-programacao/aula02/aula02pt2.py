b36d1ac76f8ff3eead539fa31ea383058f9e2a4a
Leozera2k <leofsousa98@gmail.com>
2024-09-09 08:57:04 -0300

    Update aula02pt2.py

---
+++ linguagem-programacao/aula02/aula02pt2.py
@@ -0,0 +1,8 @@
+#importe a biblioteca NumPy
+import numpy as np
+
+# crie uma array com número inteiros
+my_array = np.array([1, 2, 3, 4, 5])
+print(f'Array Original: {my_array}')
+
+#Realizando operações matemáticas com arrays
