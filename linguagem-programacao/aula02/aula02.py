b1c2049cce9069f5d74624f53dc1f0de18094fa7
Leozera2k <leofsousa98@gmail.com>
2024-09-09 08:32:49 -0300

    Update aula02.py

--- linguagem-programacao/aula02/aula02.py
+++ linguagem-programacao/aula02/aula02.py
@@ -4,3 +4,13 @@ meu_conjunto.add(10)
 meu_conjunto.add(20)
 meu_conjunto.add(30)
 print(f'Conjunto após adicionar elementos: ' , meu_conjunto)
+
+# Verificando elementos dentro do conjunto
+elemento = 20
+if elemento in meu_conjunto:
+    print(f'{elemento} está no meu conjunto!')
+else:
+    print(f'{elemento} não está no meu conjunto!')
+
+meu_conjunto.remove(elemento)
+print(f'Meu conjunto após remover o elemento {elemento}: ' , meu_conjunto)
