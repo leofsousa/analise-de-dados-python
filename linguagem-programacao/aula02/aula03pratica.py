66c7e26f75be422776dd41d225abb1bfc3774182
Leozera2k <leofsousa98@gmail.com>
2024-09-09 11:59:52 -0300

    Create aula03pratica.py

---
+++ linguagem-programacao/aula02/aula03pratica.py
@@ -0,0 +1,21 @@
+class Veiculo:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = 0
+    
+    def acelerar(self, incremento):
+        self.velocidade += incremento
+
+    def frear(self, decremento):
+        self.velocidade -= decremento
+    
+    def status(self):
+        return f'Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}, Velocidade: {self.velocidade} km/h'
+    
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, ano, potencia):
+        super().__init__(marca, modelo, ano)
+        self.potencia = potencia
+    
