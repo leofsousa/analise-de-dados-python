ac7a349c465eb0a1690aa580fc0a7b3573ff44b7
Leozera2k <leofsousa98@gmail.com>
2024-09-11 09:32:11 -0300

    update db

--- linguagem-programacao/unidade03/aula01.py
+++ linguagem-programacao/unidade03/aula01.py
@@ -18,3 +18,14 @@ cursor.execute(create_table)
 conn.execute(create_table)
 
 conn.commit()
+
+conn = sqlite3.connect('exemplo.db')
+cursor = conn.cursor()
+
+novo_produto = ('Camiseta', 19.99, 50)
+
+inserir_produto = "INSERT INTO Produtos (nome, preco, estoque) VALUES (?, ?, ?)"
+
+cursor.execute(inserir_produto, novo_produto)
+
+conn.commit()
