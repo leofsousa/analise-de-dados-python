afd07c51990eea09478e454027150c00b6f6354f
Leozera2k <leofsousa98@gmail.com>
2024-09-11 09:41:27 -0300

    update db

--- linguagem-programacao/unidade03/aula01.py
+++ linguagem-programacao/unidade03/aula01.py
@@ -19,13 +19,49 @@ conn.execute(create_table)
 
 conn.commit()
 
+#Adicionando Produto ->
+
+#conn = sqlite3.connect('exemplo.db')
+#cursor = conn.cursor()
+
+#novo_produto = ('Camiseta', 29.99, 50)
+
+#inserir_produto = "INSERT INTO Produtos (nome, preco, estoque) VALUES (?, ?, ?)"
+
+#cursor.execute(inserir_produto, novo_produto)
+
+#conn.commit()
+
+#conn.close()
+
+#Encerrando conexão
+
+#Visualizar Produto ->
+
 conn = sqlite3.connect('exemplo.db')
 cursor = conn.cursor()
 
-novo_produto = ('Camiseta', 19.99, 50)
+selecionar_produtos = "SELECT * FROM Produtos"
 
-inserir_produto = "INSERT INTO Produtos (nome, preco, estoque) VALUES (?, ?, ?)"
+cursor.execute(selecionar_produtos)
+produtos = cursor.fetchall()
+for produto in produtos:
+    print(produto)
 
-cursor.execute(inserir_produto, novo_produto)
+conn.close()
+
+#Atualizar Produto -> 
+
+conn = sqlite3.connect('exemplo.db')
+cursor = conn.cursor()
+
+novo_preco = 24.99
+produto_id = 1
+
+atualizar_preco = "UPDATE Produtos SET preco = ? WHERE id = ?"
+
+cursor.execute(atualizar_preco, (novo_preco, produto_id))
 
 conn.commit()
+
+conn.close()
